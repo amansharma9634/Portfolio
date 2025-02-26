c1f9b5734b6827301ced850c3cbe277786cf9d56
Aman <amansharma9634@gmail.com>
2025-02-26 14:02:21 +0530

    Updates

---
+++ precache-manifest.e26f016e601fed1c45ce05365c47a995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d5a01d20c226eac5152c9d20350105",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b781704583c44704ebe6",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b1ef755d0c193d78f639",
+    "url": "./static/css/main.cb786a2d.chunk.css"
+  },
+  {
+    "revision": "b781704583c44704ebe6",
+    "url": "./static/js/2.a28aad25.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.a28aad25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ef755d0c193d78f639",
+    "url": "./static/js/main.35aecf4c.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
